dcdc9595040796a8d04afcad56b77e2aa40c8505
Yurii Serediuk <yserediuk@n-ix.com>
2017-02-23 20:03:23 +0200

    Set StateListDrawable for background.

--- app/src/main/java/com/ysered/customlayoutexample/view/MyView.java
+++ app/src/main/java/com/ysered/customlayoutexample/view/MyView.java
@@ -1,12 +1,17 @@
 package com.ysered.customlayoutexample.view;
 
 import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.StateListDrawable;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.util.StateSet;
 import android.view.View;
 
 import com.ysered.customlayoutexample.view.util.ColorUtils;
 
 public class MyView extends View {
+    private static final String TAG = MyView.class.getSimpleName();
 
     public MyView(Context context) {
         super(context);
@@ -24,8 +29,27 @@ public class MyView extends View {
     }
 
     private void init(Context context) {
-        setBackgroundColor(ColorUtils.getRandomColor(context));
-        setMeasuredDimension(100, 100);
-        //setLayoutParams(new ViewGroup.LayoutParams(100, 100));
+        setMeasuredDimension(140, 140);
+
+        setEnabled(true);
+        setFocusable(true);
+        setClickable(true);
+
+        final int color = ColorUtils.getRandomColor(context);
+        final int pressedColor = ColorUtils.getGrayColor(context);
+        final int[] pressedState = { android.R.attr.state_selected, android.R.attr.state_pressed };
+
+        final StateListDrawable statesDrawable = new StateListDrawable();
+        statesDrawable.addState(StateSet.WILD_CARD, new ColorDrawable(color));
+        statesDrawable.addState(pressedState, new ColorDrawable(pressedColor));
+
+        setBackground(statesDrawable);
+
+        setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: clicked!!!");
+            }
+        });
     }
 }
